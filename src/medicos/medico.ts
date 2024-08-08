57a1d5df051526eec30811a7fbf3b80a833dc0e9
Daniel <86085164+msck1@users.noreply.github.com>
2024-08-08 10:56:41 -0300

    Atributos feitos, base do crud

--- src/medicos/medico.ts
+++ src/medicos/medico.ts
@@ -1,5 +1,39 @@
+import mysql from 'mysql2'
+import { fazerConexao } from '../db';
+
 class Medico {
 
-    // para fazer: atributos, constructor, getters e setters de acordo com o bd/der
+    private id_medico:number;
+    private nome_medico:string;
+    private especialidade_medico:string;
+    private crm_medico:string;
 
+    constructor() {
+        
+
+            // para fazer: constructor, getters e setters de acordo com o bd/der, funcoes crud
+
+        
+    }
+
+}
+
+function inserirMedico() {
+    
+}
+
+function listarMedicos() {
+    
+}
+
+function listarMedicoPorId() {
+    
+}
+
+function alterarMedico() {
+    
+}
+
+function excluirMedico() {
+    
 }
