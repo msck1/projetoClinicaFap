e103587094e26a80db28dfa0e4e4b7b3620a868e
angelicafernandes <angelicatelex@hotmail.com>
2024-08-09 20:59:08 -0300

    Update medico1.ts
    incluido constructor, gets e sets

--- src/medicos/medico.ts
+++ src/medicos/medico.ts
@@ -1,22 +1,53 @@
+//SISTEMA PARA IMPLEMENTAR E GFERENCIAAR OS MÉDICOS, PACIENTES E CONSULTAS DE UMA CLÍNICA.
+//CÓDIGO PARA CLASSE DE TELEFONE DE PACIENTE PARA O PROGRAMA DA CLINICA FAP
+
+const readline = require('readline-sync');//permitir a interação síncrona com o usuário através do console
+
 import mysql from 'mysql2'
 import { fazerConexao } from '../db';
 
 class Medico {
 
-    //private id_medico:number;
-    //private nome_medico:string;
-    //private especialidade_medico:string;
-    //private crm_medico:string;
+    private id_medico:number;
+    private nome_medico:string;
+    private especialidade_medico:string;
+    private crm_medico:string;
 
-    constructor() {
+        constructor(_id_medico:number, _nome_medico:string, _especialidade_medico:string, _crm_medico:string) {
+        this.id_medico =_id_medico
+        this.nome_paciente = _nome_paciente
+        this.especialidade_medico = _especialidade_medico
+        this.crm_medico = _crm_medico
+        }
         
-
-            // para fazer: constructor, getters e setters de acordo com o bd/der, funcoes crud
-
-        
-    }
-
-}
+        //Setters
+            setid_medico(_id_medico){
+            this.id_medico = _id_medico
+        }
+            setnome_medico(_nome_medico) {
+            this.nome_medico = _nome_medico
+        }
+            setespecialidade_medico(_especialidade_medico) {
+            this.especialidade_medico = _especialidade_medico
+        }
+            setcrm_medico(_crm_medico) {
+            this.crm_medico = _crm_medico
+        }
+        //Getters
+            getidnome_medico(){
+            return this.id_nome
+        }
+            getnome_paciente() {
+            return this.nome_paciente
+        }
+            getespecialidade_paciente() {
+            return this.especialidade_paciente
+        }
+            getespecialidade_paciente() {
+            return this.crm_paciente
+        }
+        }
+            
 
 // funcao de inserir, trocar por a query.() depois para os metods get, parametro da funcao inserirMedico 
 function inserirMedico(nome_medico:string, especialidade_medico:string, crm_medico:string, callback:(erro: mysql.QueryError | null, resultado?:any) =>  void) {
@@ -67,3 +98,51 @@ function excluirMedicoPeloCrm() {
 
 export { inserirMedico, listarMedicoPorCrm };
 export { Medico }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
